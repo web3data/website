84d1d56a13e8682318da393b4c140b9877166106
Trevor Clarke <besnoid@gmail.com>
2018-09-25 08:29:05 -0700

    remove chalk for testing

--- scripts/build.js
+++ scripts/build.js
@@ -5,7 +5,6 @@ process.env.NODE_ENV = 'production'
 var ora = require('ora')
 var rm = require('rimraf')
 var path = require('path')
-var chalk = require('chalk')
 var webpack = require('webpack')
 var config = require('../config/webpack')
 var webpackConfig = require('./webpack.prod.conf')
@@ -29,16 +28,10 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     )
 
     if (stats.hasErrors()) {
-      console.log(chalk.red('  Build failed with errors.\n'))
+      console.log('Build failed with errors.')
       process.exit(1)
     }
 
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(
-      chalk.yellow(
-        '  Tip: built files are meant to be served over an HTTP server.\n' +
-          "  Opening index.html over file:// won't work.\n",
-      ),
-    )
+    console.log('Build complete.')
   })
 })
